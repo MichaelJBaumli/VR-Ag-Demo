edb86c0b3d5f381960ec230e9451a5da4f989570
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2022-03-22 16:14:36 -0500

    Lighting changes, environment enhancements

--- VR-Ag-Demo-Godot/Low_Poly_Barn.gd
+++ VR-Ag-Demo-Godot/Low_Poly_Barn.gd
@@ -7,21 +7,22 @@ onready var matPlanksOld = preload("res://assets/Low_Poly_Barn/Low Poly Barn/Pla
 
 
 func _ready():
-	$RootNode/Model/Group1/Group2/Mesh1.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group3/Mesh2.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Difference/Mesh3.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group4/Mesh4.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group5/Mesh5.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group6/Mesh6.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group7/Mesh7.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group8/Mesh8.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group9/Mesh9.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group10/Mesh10.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group11/Mesh11.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group12/Mesh12.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group13/Mesh13.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group14/Mesh14.set_surface_material(1, matPlanksNew)
-	$RootNode/Model/Group1/Group15/Mesh15.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group2/Mesh1.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group3/Mesh2.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Difference/Mesh3.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group4/Mesh4.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group5/Mesh5.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group6/Mesh6.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group7/Mesh7.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group8/Mesh8.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group9/Mesh9.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group10/Mesh10.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group11/Mesh11.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group12/Mesh12.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group13/Mesh13.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group14/Mesh14.set_surface_material(1, matPlanksNew)
+	#$RootNode/Model/Group1/Group15/Mesh15.set_surface_material(1, matPlanksNew)'''
+	pass
 	
 
 
