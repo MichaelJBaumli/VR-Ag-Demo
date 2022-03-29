a8c14d252d52f6a9cf642f518046685afb557bdf
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2022-03-29 15:57:19 -0500

    added standin for interactive video

--- VR-Ag-Demo-Godot/Main.gd
+++ VR-Ag-Demo-Godot/Main.gd
@@ -1,6 +1,8 @@
 extends Node
 
-
+onready var player = $PlayerMovement
+onready var playerrect = $PlayerMovement/MeshInstance
+onready var timer = $Timer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,7 +10,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +19,9 @@ func _ready():
 
 
 func _on_Area_area_entered(area):
+	#black out:::
+	playerrect.visible = true
+	timer.start()
 	match area.type:
 		"cheese":# For cheese, carton, butter and icecream, (and later yogurt) will change scenes here depending on what object enters
 			pass
@@ -29,3 +34,8 @@ func _on_Area_area_entered(area):
 		"crate":
 			#DO NOTING DONT CHANGE SCENE THIS ONLY NEEDED A TYPE BECAUSE IT IS PICKABLE
 			pass
+
+
+
+func _on_Timer_timeout():
+	playerrect.visible = false
